7cc2a5cbec948bb583cb35394dc15dd3c4b9957d
Marcus Povey <marcus@marcus-povey.co.uk>
2014-11-22 12:38:30 +0000

    Ability to save signed HTTP tokens

--- Known/Pages/Account.php
+++ Known/Pages/Account.php
@@ -1,50 +1,55 @@
 <?php
 
+/**
+ * Known pages
+ */
+
+namespace IdnoPlugins\Known\Pages {
+
     /**
-     * Known pages
+     * Default class to serve Known-related account settings
      */
+    class Account extends \Idno\Common\Page {
 
-    namespace IdnoPlugins\Known\Pages {
+	function getContent() {
+	    $this->gatekeeper(); // Logged-in users only
 
-        /**
-         * Default class to serve Known-related account settings
-         */
-        class Account extends \Idno\Common\Page
-        {
+	    if ($known = \Idno\Core\site()->plugins()->get('Known')) {
+		if (!$known->hasKnown()) {
+		    if ($knownAPI = $known->connect()) {
+			$login_url = $knownAPI->getAuthenticationUrl(
+				\Idno\Core\site()->config()->known['site'] . 'oauth2/authorise', ['response_type' => 'code', 'state' => \IdnoPlugins\Known\Main::getState(), 'redirect_uri' => \IdnoPlugins\Known\Main::getRedirectUrl()]
+			);
+		    }
+		} else {
+		    $login_url = '';
+		}
+	    }
+	    $t = \Idno\Core\site()->template();
+	    $body = $t->__(['login_url' => $login_url])->draw('account/known');
+	    $t->__(['title' => 'Known Crosspost', 'body' => $body])->drawPage();
+	}
 
-            function getContent()
-            {
-                $this->gatekeeper(); // Logged-in users only
-		
-                if ($known = \Idno\Core\site()->plugins()->get('Known')) { 
-                    if (!$known->hasKnown()) {
-                        if ($knownAPI = $known->connect()) {
-                            $login_url = $knownAPI->getAuthenticationUrl(
-				\Idno\Core\site()->config()->known['site'] . 'oauth2/authorise',
-				['response_type' => 'code', 'state' => \IdnoPlugins\Known\Main::getState(), 'redirect_uri' => \IdnoPlugins\Known\Main::getRedirectUrl()] 
-                            );
-			    
-                        }
-                    } else {
-                        $login_url = '';
-                    }
-                }
-                $t = \Idno\Core\site()->template();
-                $body = $t->__(['login_url' => $login_url])->draw('account/known');
-                $t->__(['title' => 'Known Crosspost', 'body' => $body])->drawPage();
-            }
+	function postContent() {
+	    $this->gatekeeper(); // Logged-in users only
+	    if (($this->getInput('remove'))) {
+		$user = \Idno\Core\site()->session()->currentUser();
+		$user->known = [];
+		$user->save();
+		\Idno\Core\site()->session()->addMessage('Your Known Crosspost settings have been removed from your account.');
+	    } else {
+		$user = \Idno\Core\site()->session()->currentUser();
+		$user->known = [
+		    'username' => $this->getInput('username'),
+		    'known_api_key' => $this->getInput('known_api_key')
+		];
 
-            function postContent() {
-                $this->gatekeeper(); // Logged-in users only
-                if (($this->getInput('remove'))) {
-                    $user = \Idno\Core\site()->session()->currentUser();
-                    $user->known = [];
-                    $user->save();
-                    \Idno\Core\site()->session()->addMessage('Your Known Crosspost settings have been removed from your account.');
-                }
-                $this->forward('/account/known/');
-            }
-
-        }
+		$user->save();
+		\Idno\Core\site()->session()->addMessage('Your Known api details were saved.');
+	    }
+	    $this->forward('/account/known/');
+	}
 
     }
+
+}
--- Known/templates/default/account/known.tpl.php
+++ Known/templates/default/account/known.tpl.php
@@ -2,50 +2,70 @@
 
     <div class="span10 offset1">
         <h3>Known</h3>
-        <?=$this->draw('account/menu')?>
+	<?= $this->draw('account/menu') ?>
     </div>
 
 </div>
 <div class="row">
     <div class="span10 offset1">
         <form action="/account/known/" class="form-horizontal" method="post">
-            <?php
-                if (empty(\Idno\Core\site()->session()->currentUser()->known)) {
-            ?>
-                    <div class="control-group">
-                        <div class="controls">
-                            <p>
-                                If you have a remote Known account, you may connect it here. Public content that you
-                                post to this site will be automatically cross-posted to your Known wall.
-                            </p>
-                            <p>
-                                <a href="<?=$vars['login_url']?>" class="btn btn-large btn-success">Click here to connect Known to your account</a>
-                            </p>
-                        </div>
-                    </div>
-                <?php
+	    <?php
+	    if (empty(\Idno\Core\site()->session()->currentUser()->known)) {
+		?>
+    	    <div class="control-group">
+    		<div class="controls">
+    		    <p>
+    			If you have a remote Known account, you may connect it here. Public content that you
+    			post to this site will be automatically cross-posted to your Known wall.
+    		    </p>
+    		    <p>
+    			<a href="<?= $vars['login_url'] ?>" class="btn btn-large btn-success">Click here to connect Known to your account</a>
+    		    </p>
+    		</div>
 
-                } else {
+    		<hr />
+    	    </div>
+    	    <div class="control-group">
+    		<p>
+    		    Alternatively, you can connect using your per-user API key, which is available from your user settings. This doesn't require any extra software installed on the 
+    		    target Known installation, however you are limited to one code for ALL connecting clients, meaning you are unable to control access on a per-application basis.
+    		</p>
+    		<label class="control-label" for="name">Remote username</label>
+    		<div class="controls">
+    		    <input type="text" id="username" placeholder="Username on remote known installation" class="span4" name="username" value="<?= htmlspecialchars(\Idno\Core\site()->session()->currentUser()->known['username']) ?>" >
+    		</div>
 
-                    ?>
-                    <div class="control-group">
-                        <div class="controls">
-                            <p>
-                                Your account is currently connected to a remote Known server. Public content that you post here
-                                will be shared with your Known account.
-                            </p>
-                            <p>
-                                <input type="hidden" name="remove" value="1" />
-                                <button type="submit" class="btn btn-large btn-primary">Click here to remove Known from your account.</button>
-                            </p>
-                        </div>
-                    </div>
+    		<label class="control-label" for="name">Remote user's API Key</label>
+    		<div class="controls">
+    		    <input type="text" id="known_api_key" placeholder="Secret Key" class="span4" name="known_api_key" value="<?= htmlspecialchars(\Idno\Core\site()->session()->currentUser()->known['known_api_key']) ?>" >
+    		</div>
+    	    </div>
+	    <div class="control-group">
+                <div class="controls">
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </div>
+            </div>
 
-                <?php
+		<?php
+	    } else {
+		?>
+    	    <div class="control-group">
+    		<div class="controls">
+    		    <p>
+    			Your account is currently connected to a remote Known server. Public content that you post here
+    			will be shared with your Known account.
+    		    </p>
+    		    <p>
+    			<input type="hidden" name="remove" value="1" />
+    			<button type="submit" class="btn btn-large btn-primary">Click here to remove Known from your account.</button>
+    		    </p>
+    		</div>
+    	    </div>
 
-                }
-            ?>
-            <?= \Idno\Core\site()->actions()->signForm('/account/known/')?>
+		<?php
+	    }
+	    ?>
+	    <?= \Idno\Core\site()->actions()->signForm('/account/known/') ?>
         </form>
     </div>
 </div>
