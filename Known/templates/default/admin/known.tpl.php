b863d3664ff1dd06d37eda43ee0b7fe71ace4059
Marcus Povey <marcus@marcus-povey.co.uk>
2015-09-04 13:24:33 +0100

    Bootstrap 3 - oops

--- Known/templates/default/admin/known.tpl.php
+++ Known/templates/default/admin/known.tpl.php
@@ -1,13 +1,13 @@
 <div class="row">
 
-    <div class="col-offset-10 col-md-offset-1">
+    <div class="col-md-10 col-md-offset-1">
         <h1>Known</h1>
 	<?= $this->draw('admin/menu') ?>
     </div>
 
 </div>
 <div class="row">
-    <div class="col-offset-10 col-md-offset-1">
+    <div class="col-md-10 col-md-offset-1">
         <form action="/admin/known/" class="form-horizontal" method="post">
             <div class="control-group">
                 <div class="controls">
@@ -19,7 +19,7 @@
 	    <div class="control-group">
                 <label class="control-label" for="name">Known site URL</label>
                 <div class="controls">
-                    <input type="url" id="name" placeholder="Site url" class="col-offset-4" name="site" value="<?= htmlspecialchars(\Idno\Core\site()->config()->known['site']) ?>" required >
+                    <input type="url" id="name" placeholder="Site url" class="col-md-4" name="site" value="<?= htmlspecialchars(\Idno\Core\site()->config()->known['site']) ?>" required >
                 </div>
 
 		<hr />
@@ -32,13 +32,13 @@
 		</div>
                 <label class="control-label" for="name">Client ID</label>
                 <div class="controls">
-                    <input type="text" id="name" placeholder="App Key" class="col-offset-4" name="appId" value="<?= htmlspecialchars(\Idno\Core\site()->config()->known['appId']) ?>" >
+                    <input type="text" id="name" placeholder="App Key" class="col-md-4" name="appId" value="<?= htmlspecialchars(\Idno\Core\site()->config()->known['appId']) ?>" >
                 </div>
             </div>
             <div class="control-group">
                 <label class="control-label" for="name">Client Secret</label>
                 <div class="controls">
-                    <input type="text" id="name" placeholder="Secret Key" class="col-offset-4" name="secret" value="<?= htmlspecialchars(\Idno\Core\site()->config()->known['secret']) ?>" >
+                    <input type="text" id="name" placeholder="Secret Key" class="col-md-4" name="secret" value="<?= htmlspecialchars(\Idno\Core\site()->config()->known['secret']) ?>" >
                 </div>
             </div>
             <div class="control-group">
